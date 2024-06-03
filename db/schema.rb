08062de446908bca035c78d32d3fbef3be083e80
Mathieu Rabet <rabet.mathieu@gmail.com>
2024-06-03 18:26:00 +0200

    Add navbar, title and picture
    Signed-off-by: Mathieu Rabet <rabet.mathieu@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_31_090240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
